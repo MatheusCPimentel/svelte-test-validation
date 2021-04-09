29be0ae6c7593f4098730f828b669fd950b2a145
Matheus Pimentel <matheus08.pimentel@gmail.com>
2021-04-09 11:16:26 -0300

    First commit

---
+++ src/App.old.svelte
@@ -0,0 +1,50 @@
+<script>
+	let name;
+	let email;
+	let password; 
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		console.log('Nome:', name);
+		console.log('E-mail:', email);
+		console.log('Password:', password);
+	}
+
+</script>
+
+<main>
+	<form>
+		<div>
+			<label>Nome</label>
+			<input bind:value={name} placeholder="Digite o nome aqui..." />
+		</div>
+		
+		<div>
+			<label>E-mail</label>
+			<input bind:value={email} placeholder="Digite o e-mail aqui..." />
+		</div>
+		
+		<div>
+			<label>Senha</label>
+			<input type="password" bind:value={password} placeholder="Digite a senha aqui..." />
+		</div>
+
+		<button type="submit" on:click={handleSubmit}>Submit</button>
+	</form>
+</main>
+
+<style>
+	form {
+		margin: 10px;
+	}
+
+	label {
+		margin-bottom: 5px;
+	}
+
+	input {
+		margin-bottom: 20px;
+	}
+
+</style>
---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { form, bindClass } from 'svelte-forms';
+
+  let name = '';
+
+  const myForm = form(() => ({
+    name: { value: name, validators: ['required'] }
+  }));
+</script>
+
+<style>
+  :global(input.invalid) {
+    border-color: red;
+  }
+</style>
+
+<form>
+  <input type="text" name="name" bind:value={name} use:bindClass={{ form: myForm }} />
+
+  <button disabled="{!$myForm.valid}">Login</button>
+</form>
